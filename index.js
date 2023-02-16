f9b820ee22484041be7d6f62264b85fff5e25dd7
CF255 <andrewsluis2011@gmail.com>
2023-02-16 14:57:45 -0400

    creacion de productos

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// require("./lib/server")
-const exp = require('express')
-const cluster = require('cluster')
-const cors = require('cors')
-const numCpu = require('os').cpus().length;
-require('dotenv').config();
-if(cluster.isMaster){
-    for(let i = 0; i < numCpu; i++){
-        cluster.fork();
-    }
-}else{
-    const app = exp();
-    app.use(exp.json());
-    app.use(cors());
-    const routes = require('./routes/routes');
-    app.use(routes);
-    app.listen(3005, ()=>{
-        console.log('Listening in port 3005');
-    })
-}
+require("./lib/server")
+// const exp = require('express')
+// const cluster = require('cluster')
+// const cors = require('cors')
+// const numCpu = require('os').cpus().length;
+// require('dotenv').config();
+// if(cluster.isMaster){
+//     for(let i = 0; i < numCpu; i++){
+//         cluster.fork();
+//     }
+// }else{
+//     const app = exp();
+//     app.use(exp.json());
+//     app.use(cors());
+//     const routes = require('./routes/routes');
+//     app.use(routes);
+//     app.listen(3005, ()=>{
+//         console.log('Listening in port 3005');
+//     })
+// }
