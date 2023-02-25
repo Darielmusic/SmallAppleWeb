a60d5127e8f1fa1a05afd1bf52dadc7b81503394
Dariel Salas <darielmusic24@gmail.com>
2023-02-24 20:09:45 -0400

    SYNC

--- controller/products.controller.js
+++ controller/products.controller.js
@@ -1,27 +1,28 @@
 const models = require('../models/products.model')
+const global = require('../models/global.modal')
 const { connection } = require('../db/dbconnections')
 
 module.exports = {
 
-    getProduct: async function(req, res){
+    getProduct: async function (req, res) {
         let pool = await connection;
 
         let resultado = await pool
-        .request()
-        .query(
-            `select id, ltrim(rtrim(article))article,ltrim(rtrim(barcode))barcode,ltrim(rtrim(description))description, statusId, categoryId, price from produts`
-        );
+            .request()
+            .query(
+                `select id, ltrim(rtrim(article))article,ltrim(rtrim(barcode))barcode,ltrim(rtrim(description))description, statusId, categoryId, price from produts`
+            );
         // let dto = modelValidation(models.product, resultado.recordset);
         res.json(resultado.recordset);
     },
 
-    getById: async function(req, res){
-        const {id} = req.params;
+    getById: async function (req, res) {
+        const { id } = req.params;
         let pool = await connection;
         let result = await pool.request()
-        .query(`select id,ltrim(rtrim(article)) article, ltrim(rtrim(barcode)) barcode, ltrim(rtrim(description))description, statusId, categoryId, price from produts where id = ${id}`)
-        let respuesta = result.recordset.map((db)=>{
-            let dto = {...models.product};
+            .query(`select id,article, barcode, description, statusId, categoryId, price from produts where id = ${id}`)
+        let respuesta = result.recordset.map((db) => {
+            let dto = { ...models.product };
             dto.id = db.id
             dto.article = db.article
             dto.barcode = db.barcode
@@ -36,7 +37,7 @@ module.exports = {
         return res.json(respuesta);
     },
 
-    postProduct: async function(req, res){
+    postProduct: async function (req, res) {
         try {
             const {
                 article,
@@ -50,18 +51,32 @@ module.exports = {
             let pool = await connection;
 
             let result = await pool
-            .request()
-            .input("article", article)
-            .input("barcode", barcode)
-            .input("description", description)
-            .input("statusId", statusId)
-            .input("categoryId", categoryId)
-            .input("price", price)
-            .execute("usp_crearProductos")
+                .request()
+                .input("article", article)
+                .input("barcode", barcode)
+                .input("description", description)
+                .input("statusId", statusId)
+                .input("categoryId", categoryId)
+                .input("price", price)
+                .execute("dbo.usp_crearProductos")
 
-           return res.json(result.recordset);
+            let respuesta = result.recordset.map((db) => {
+                let dto = { ...global.modelReponse };
+                dto.status = db.status
+                dto.message = db.message
+                return dto;
+            })
+          
+            if (respuesta[0].status == 3) {
+                return res.json(req.body);
+            } else {
+                console.log(respuesta);
+                throw new Error(respuesta[0].message)
+            }
+           
         } catch (error) {
-            
+            console.log(error);
+            return res.status(400).json(error)
         }
     }
 }
---
+++ models/global.modal.js
@@ -0,0 +1,8 @@
+let modelReponse ={
+    status: null,
+    message: null
+}
+
+module.exports ={
+    modelReponse
+}
